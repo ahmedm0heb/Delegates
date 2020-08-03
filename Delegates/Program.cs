f0035e5a3b893cc485440c21cac1131d624afcf2
ahmedm0heb <moheb413@gmail.com>
2020-08-03 23:04:22 +0100

    Add project files.

---
+++ Delegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class myTestClass
+    {
+
+        public string TextLength(string text)
+        {
+            return "number of text is " + text.Length;
+        }
+    }
+    class Program
+    {
+        public delegate string FirDelgate(string text);
+
+
+        static void Main(string[] args)
+        {
+            myTestClass newtest = new myTestClass();
+
+            FirDelgate lengthoftext = new FirDelgate(newtest.TextLength);
+
+            Console.WriteLine("enter text");
+
+            string text = Console.ReadLine();
+
+            Console.Write(lengthoftext(text));
+        }
+    }
+}
+
